07b1462e13fb45b811457062b6e52947a085c6b4
Julien <Julien@JAH-Computer.local>
2016-05-28 11:04:47 +0200

    v1 titanic api

--- apython.js
+++ apython.js
@@ -1,14 +1,74 @@
+// call the packages we need
+var express    = require('express');
+var bodyParser = require('body-parser');
+var morgan     = require('morgan');
 var PythonShell = require('python-shell');
+var app        = express();
 
-var options = {
-  mode: 'text',
-  scriptPath: 'scripts',
-  args: [2, 5]
-};
+// configure app
+app.use(morgan('dev')); // log requests to the console
 
-PythonShell.run('script.py', options, function (err, results) {
-  if (err) throw err;
-  // results is an array consisting of messages collected during execution
-  console.log(results[0]);
+// configure body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port     = process.env.PORT || 9000; // set our port
+
+// ROUTES FOR OUR API
+// create our router
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next();
 });
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'Welcome to the Titanic Prediction API!' });	
+});
+
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/api', router);
+
+router.route('/titanic')
+
+	// 
+	.post(function(req, res) {
+			
+		var array = JSON.parse(req.body.args);
+		
+		var options = {
+		  mode: 'text',
+		  scriptPath: 'scripts',
+		  args: array
+		};
+
+		PythonShell.run('script.py', options, function (err, results) {
+		  if (err) throw err;
+		  // results is an array consisting of messages collected during execution
+		  res.json({ message : results[0] })
+		});
+		
+
+		
+	});
+
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+
+
+
+
+
+
+
+
 	
--- scripts/script.py
+++ scripts/script.py
@@ -1,7 +1,34 @@
 import sys
+import numpy as np
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
 
-# simple argument echo script
+# import data
+data = pd.read_csv('scripts/titanic_train.csv')
+
+# extract target
+survived_column = data['Survived']
+target = survived_column.values
+
+# extract features
+numerical_features = data.get(['Fare', 'Pclass', 'Age'])
+median_features = numerical_features.dropna().median()
+imputed_features = numerical_features.fillna(median_features)
+features_array = imputed_features.values
+
+# split database
+features_train, features_test, target_train, target_test = train_test_split(features_array, target, test_size=0.20, random_state=0)
+
+# fitting
+logreg = LogisticRegression(C=1.)
+logreg.fit(features_train, target_train)
+
+# get array
 a = int(float(sys.argv[1]))
 b = int(float(sys.argv[2]))
+c = int(float(sys.argv[3]))
 
-print(a + b)
+# print prediction
+print(logreg.predict([[a,b,c]]))
+
